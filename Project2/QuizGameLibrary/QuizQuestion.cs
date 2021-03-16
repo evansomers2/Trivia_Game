261327897dc7952196dcd7d16b287dba6b22f86c
evansomers2 <evansomers2@gmail.com>
2021-03-16 18:21:31 -0400

    Added csv data for questions and parsing methods

--- Project2/QuizGameLibrary/QuizQuestion.cs
+++ Project2/QuizGameLibrary/QuizQuestion.cs
@@ -1,28 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace QuizGameLibrary
 {
+    [DataContract]
     public class QuizQuestion
     {
+        [DataMember]
         public string Question { get; set; }
-        private string Answer { get; set; }
+        [DataMember]
+        private string AnswerA { get; set; }
+        [DataMember]
+        private string AnswerB { get; set; }
+        [DataMember]
+        private string AnswerC { get; set; }
+        [DataMember]
+        private string AnswerD { get; set; }
 
-        public QuizQuestion(string question, string answer)
+        private string CorrectAnswer { get; set; }
+
+        public QuizQuestion(string question, string answera, string answerb, string answerc, string answerd, string correct)
         {
             Question = question;
-            Answer = answer;
+            AnswerA = answera;
+            AnswerB = answerb;
+            AnswerC = answerc;
+            AnswerD = answerd;
+            CorrectAnswer = correct;
         }
 
         public bool CheckAnswer(string answer)
         {
-            if (answer == Answer)
-                return true;
-            else
-                return false;
+            switch (CorrectAnswer)
+            {
+                case "A":
+                    if (AnswerA == answer) return true; else return false;
+                case "B":
+                    if (AnswerB == answer) return true; else return false;
+                case "C":
+                    if (AnswerC == answer) return true; else return false;
+                case "D":
+                    if (AnswerD == answer) return true; else return false;
+                default:
+                    return false;
+
+            }
         }
 
     }
